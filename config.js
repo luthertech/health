00eded4c8bb5a6cb9fd18711fc22f414289fa700
Hassaan Tariq <mrhassantariq17@gmail.com>
2025-09-09 01:46:14 +0500

    Fixed multiple issues

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+window.APP_CONFIG = {
+  ENV: "production",
+  STAGING: {
+    API_BASE: "https://luther-tech-health-pdmd.vercel.app",
+    SITE_BASE: "https://luther-tech-health.vercel.app",
+  },
+  PRODUCTION: {
+    API_BASE: "https://luther-tech-health-pdmd.vercel.app",
+    SITE_BASE: "https://www.luthertech.co.uk",
+  },
+};
